dd2381499e8fe29fd8978e0814abbd65bc27b160
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-08-07 10:01:18 +0200

    made changes in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 var buttons = document.getElementsByClassName("button"),
     list = document.getElementById("list"),
     btnLength = buttons.length,
-    frag = document.createDocumentFragment();
+    frag = document.createDocumentFragment(),
+    li, i;
 
-for (var i = 0; i < btnLength; i++) {
-    var li = document.createElement("li");
-    li.innerHTML = (buttons[i].innerText);
+for (i = 0; i < btnLength; i++) {
+    li = document.createElement("li");
+    li.textContent = buttons[i].innerText;
     frag.appendChild(li);
 }
 list.appendChild(frag);
